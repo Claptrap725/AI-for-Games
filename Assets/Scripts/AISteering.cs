1ac34e63ac1508387e852b79f6f98f3e80cb8113
Tristinnewby <tristinnewby@outlook.com>
2020-02-10 12:18:50 -0800

    Finished seek, flee, wander

--- Assets/Scripts/AISteering.cs
+++ Assets/Scripts/AISteering.cs
@@ -4,5 +4,60 @@ using UnityEngine;
 
 public class AISteering : MonoBehaviour
 {
-    
+    public List<GameObject> seekTargets;
+    public List<GameObject> fleeTargets;
+    public List<GameObject> pursueTargets;
+    public List<GameObject> evadeTargets;
+    Vector3 tempSeekTarget = Vector3.zero;
+    public bool wander;
+    public bool keepHeading;
+    public float wanderTolerance = 0.2f;
+    public float wanderDistance = 1;
+    public float speed;
+
+    Vector3 velocity = new Vector3();
+    Vector3 heading = new Vector3();
+
+    private void Update()
+    {
+        Vector3 force = new Vector3();
+        if (wander)
+        {
+            if (tempSeekTarget == Vector3.zero)
+            {
+                Vector3 randDir = Random.onUnitSphere;
+                if (keepHeading)
+                    randDir += heading;
+                randDir *= wanderDistance;
+                randDir = new Vector3(randDir.x, 0, randDir.z);
+                randDir += transform.position;
+                tempSeekTarget = randDir;
+            }
+            else
+            {
+                force += ((tempSeekTarget - transform.position).normalized * speed);
+
+                Vector3 vToTarget = tempSeekTarget - transform.position;
+                float distanceToTarget = Mathf.Sqrt(vToTarget.x * vToTarget.x + vToTarget.y * vToTarget.y);
+                if (distanceToTarget < wanderTolerance)
+                {
+                    tempSeekTarget = Vector3.zero;
+                }
+            }
+        }
+        for (int i = 0; i < seekTargets.Count; i++)
+        {
+            force += ((seekTargets[i].transform.position - transform.position).normalized * speed);
+        }
+        for (int i = 0; i < fleeTargets.Count; i++)
+        {
+            force += ((transform.position - fleeTargets[i].transform.position).normalized * speed);
+        }
+        force -= velocity;
+        
+
+        velocity += force * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        heading = velocity.normalized;
+    }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        Vector3 movement = new Vector3();
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movement += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movement += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movement += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement += Vector3.left;
+        }
+        movement.Normalize();
+        movement *= speed * Time.deltaTime;
+        transform.position += movement;
+    }
+}
