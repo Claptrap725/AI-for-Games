310f0c8b997ffcea194e1f3e11f7270f303872d5
Tristinnewby <tristinnewby@outlook.com>
2020-02-10 15:42:23 -0800

    pursue, evade, avoid walls

--- Assets/Scripts/AISteering.cs
+++ Assets/Scripts/AISteering.cs
@@ -10,54 +10,90 @@ public class AISteering : MonoBehaviour
     public List<GameObject> evadeTargets;
     Vector3 tempSeekTarget = Vector3.zero;
     public bool wander;
-    public bool keepHeading;
+    public float headingDistance;
     public float wanderTolerance = 0.2f;
     public float wanderDistance = 1;
-    public float speed;
+    float wanderTimer = 0;
+    public float maxSpeed;
 
-    Vector3 velocity = new Vector3();
+    Rigidbody rb;
     Vector3 heading = new Vector3();
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         Vector3 force = new Vector3();
         if (wander)
         {
+            if (wanderTimer <= 0)
+            {
+                tempSeekTarget = Vector3.zero;
+            }
             if (tempSeekTarget == Vector3.zero)
             {
                 Vector3 randDir = Random.onUnitSphere;
-                if (keepHeading)
-                    randDir += heading;
+                randDir += heading * headingDistance;
                 randDir *= wanderDistance;
                 randDir = new Vector3(randDir.x, 0, randDir.z);
                 randDir += transform.position;
                 tempSeekTarget = randDir;
+                wanderTimer = 0.5f;
             }
             else
             {
-                force += ((tempSeekTarget - transform.position).normalized * speed);
+                wanderTimer -= Time.deltaTime;
+                force += (tempSeekTarget - transform.position).normalized;
 
                 Vector3 vToTarget = tempSeekTarget - transform.position;
-                float distanceToTarget = Mathf.Sqrt(vToTarget.x * vToTarget.x + vToTarget.y * vToTarget.y);
+                float distanceToTarget = Mathf.Sqrt(vToTarget.x * vToTarget.x + vToTarget.z * vToTarget.z);
                 if (distanceToTarget < wanderTolerance)
                 {
                     tempSeekTarget = Vector3.zero;
                 }
             }
         }
+
         for (int i = 0; i < seekTargets.Count; i++)
         {
-            force += ((seekTargets[i].transform.position - transform.position).normalized * speed);
+            force += (seekTargets[i].transform.position - transform.position).normalized;
         }
         for (int i = 0; i < fleeTargets.Count; i++)
         {
-            force += ((transform.position - fleeTargets[i].transform.position).normalized * speed);
+            force += (transform.position - fleeTargets[i].transform.position).normalized;
         }
-        force -= velocity;
-        
 
-        velocity += force * Time.deltaTime;
-        transform.position += velocity * Time.deltaTime;
-        heading = velocity.normalized;
+        for (int i = 0; i < pursueTargets.Count; i++)
+        {
+            force += (pursueTargets[i].transform.position + pursueTargets[i].GetComponent<Rigidbody>().velocity - transform.position).normalized;
+        }
+        for (int i = 0; i < evadeTargets.Count; i++)
+        {
+            force += (transform.position - evadeTargets[i].transform.position + evadeTargets[i].GetComponent<Rigidbody>().velocity).normalized;
+        }
+
+        Collider[] walls = Physics.OverlapSphere(heading.normalized + transform.position, 1);
+        for (int i = 0; i < walls.Length; i++)
+        {
+            if (walls[i].tag == "Wall")
+            force += (transform.position - walls[i].transform.position).normalized*2; 
+        }
+
+        rb.velocity += force;
+        float speed = Mathf.Sqrt(rb.velocity.x * rb.velocity.x + rb.velocity.z * rb.velocity.z);
+        if (speed > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        heading = rb.velocity.normalized;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(heading.normalized + transform.position, 1);
     }
 }
